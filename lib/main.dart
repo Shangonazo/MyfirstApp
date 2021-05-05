083a33c9be60240b160c862d777857909eb73510
Robert <roberto_rivera09@hotmail.com>
2021-05-05 06:52:51 -0500

    archivo de dart

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MI primera aplicación movil',
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hola mundo, ésta es mi primer App'),
+      ),
+    );
+  }
+}
